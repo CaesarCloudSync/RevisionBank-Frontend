0d1bd93615b0f281350d743596bf509ceb98e6ef
amari <amari.lawal@gmail.com>
2023-12-07 19:30:46 +0000

    change homepage wording

--- src/components/homepage/components/aboutsection.tsx
+++ src/components/homepage/components/aboutsection.tsx
@@ -8,9 +8,11 @@ export default function AboutSection() {
             <h1>Welcome To RevisionBank</h1>
             </div>
         </div>
-        <div className="container" data-aos="fade-up" data-aos-duration="1000">
+        <div style={{marginTop:"20px"}} className="container" data-aos="fade-up" data-aos-duration="1000">
             <p className='about-text'>
-            RevisionBank was founded with the goal to add convenience to your revision. We can provide you with exam papers and the respective question papers and markschemes. Revision material, made in the form of revision cards can be sent straight to your email. These revision materials can be scheduled to be sent every minute,day,week and month. Implementing the use of the well researched active recall methods. Revision material can also be shared with other users, allowing them to review and revise the same material. The RevisionBank Snapshot chrome extension will also allow for revision material to be stored in your revision cards from your browser by cropping the selected text on a webpage.
+            RevisionBank was founded with the goal to add convenience to your revision and notetaking. Create revision cards and personal notes, accompanied with images and gifs. Then schedule them to be sent straight to your email. Your cards can be scheduled to be sent every minute,day,week and month. 
+            Implementing the use of the well researched active recall methods. Revision cards and notes can also be shared with other users using a revisionbank link url, allowing them to review and revise the same material. 
+            
             </p>
                
         
